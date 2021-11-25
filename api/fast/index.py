0fb28e083ec122235e800e6c4426a33e5b62a803
JB-Tellez <jb.tellez@gmail.com>
2021-11-25 10:10:28 -0800

    adds another fast path

--- api/fast/index.py
+++ api/fast/index.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get("/api/fast")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
