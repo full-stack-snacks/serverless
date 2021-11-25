4156ecc5e5254fc0dcf0f075d6676652ccf1a7da
JB-Tellez <jb.tellez@gmail.com>
2021-11-25 10:20:52 -0800

    adds git push

--- api/fast/index.py
+++ api/fast/index.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/api/fast")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!"}
 
 @app.get("/api/fast/items/{item_id}")
 async def read_item(item_id):
