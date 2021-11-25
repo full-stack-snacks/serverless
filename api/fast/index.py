99e9474b678ec9fac5c99cfa1535eb908b8af80f
JB-Tellez <jb.tellez@gmail.com>
2021-11-25 10:27:28 -0800

    adds path segments to fast

--- api/fast/index.py
+++ api/fast/index.py
@@ -6,7 +6,3 @@ app = FastAPI()
 @app.get("/api/fast")
 async def root():
     return {"message": "Hello World!"}
-
-@app.get("/api/fast/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
